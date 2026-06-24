f0c9ac0c01e18f3c0260ac736fa73547214e35a4
NathanVeiga <nathanveiga@edu.unifil.br>
2026-06-24 20:55:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// Espera a página HTML carregar completamente
+//Espera a página HTML carregar completamente
 document.addEventListener("DOMContentLoaded", function() {
 
-    // Captura o formulário do HTML usando o ID
+    //Captura o formulário do HTML usando o ID
     const formulario = document.getElementById("meuFormulario");
 
-    // O "if" garante que a função só rode se o formulário existir na página (no caso, a tela de contato)
+    //O if garante que a função só rode se o formulário existir na página
     if (formulario) {
         
-        // REQUISITO: Função que escuta quando o usuário clica em enviar (Submit)
+        //Função que escuta quando o usuário clica em enviar
         formulario.addEventListener("submit", function(event) {
             
             // Impede a página de recarregar sozinha ao clicar no botão
@@ -16,13 +16,13 @@ document.addEventListener("DOMContentLoaded", function() {
             // MANIPULAÇÃO DO DOM 1: Captura o texto digitado no input de nome
             const nomeDigitado = document.getElementById("nome").value;
             
-            // MANIPULAÇÃO DO DOM 2: Altera um parágrafo escondido para mostrar a mensagem de sucesso
+            //Altera um parágrafo escondido para mostrar a mensagem de sucesso
             const campoMensagem = document.getElementById("mensagemSucesso");
             campoMensagem.textContent = "Sucesso! Obrigado pelo envio, " + nomeDigitado + ".";
-            campoMensagem.style.display = "block"; // Altera o CSS via JS para fazer aparecer
+            campoMensagem.style.display = "block";
             
-            // Dispara um alerta nativo simples
+            //Dispara um alerta nativo simples
             alert("Formulário processado via JavaScript!");
         });
     }
-});
+});w1
